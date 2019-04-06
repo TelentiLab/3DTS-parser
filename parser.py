c4bc7af09d73176ef3a7126e66aaa784f16788ab
Li Yin <liyin@scripps.edu>
2019-04-05 18:51:29 -0700

    Added a parser to produce formatted score tsv file

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import json
 import time
 import gzip
+import datetime
 from typing import Dict
 
 _score_file_path = '/Users/yin.li/Downloads/archive/scores.js'
@@ -8,6 +9,52 @@ _loci_file_path = '/Users/yin.li/Downloads/archive/structuralFeatures.loci.js.gz
 SCORE_LINES = 2273865
 LOCI_LINES = 413021682
 FEATURE_KEYS = ['pdbId', 'pdbChain', 'uniprotFeatureName', 'pdbResidueMin', 'pdbResidueMax']
+"""
+"featureKey": { # this whole object is the key in this file
+    "pdbId": "10GS",
+    "pdbChain": "A",
+    "uniprotFeatureName": "HELIX",
+    "pdbResidueMin": "187",
+    "pdbResidueMax": "194"
+}
+"""
+
+
+def parse_scores(filename: str, total_lines: int = 0) -> None:
+    start_time = time.time()
+    counter = 0
+    skipped_lines = []
+    with open('./formatted_scores.tsv', 'w') as file_out:
+        with open(filename, 'r') as file:
+            for line in file:
+                try:
+                    data: Dict = json.loads(line.strip())
+                except:  # error parsing json
+                    skipped_lines.append(line)
+                    continue
+
+                feature = data.get('featureKey')
+                if not feature:
+                    skipped_lines.append(line)
+                    continue
+                key = '.'.join([feature[field] for field in FEATURE_KEYS])
+                try:
+                    post_global_synonymous_rate = data.get(
+                        'nsPostHeptamerIndependentChromosomeSpecificIntergenicRate'
+                    ).get('mean')
+                except AttributeError:  # failed to find score
+                    skipped_lines.append(line)
+                    continue
+                file_out.write(f'{key}\t{post_global_synonymous_rate}\n')
+                if total_lines:
+                    counter += 1
+                    if counter % (total_lines // 1000) == 0:
+                        ratio = counter / total_lines
+                        time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
+                        print(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left}')
+        end_time = time.time()
+        print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
+        print(f'formatted score created, time spent: {end_time - start_time}')
 
 
 def read_score_into_ram(filename: str, total_lines: int = 0) -> Dict:
@@ -46,9 +93,10 @@ def read_score_into_ram(filename: str, total_lines: int = 0) -> Dict:
             score_map[key] = post_global_synonymous_rate
             if total_lines:
                 counter += 1
-                ratio = counter / total_lines
-                time_left = (time.time() - start_time) * (1 - ratio) / ratio
-                print(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left:0.0f}s')
+                if counter % (total_lines // 1000) == 0:
+                    ratio = counter / total_lines
+                    time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
+                    print(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left}')
     end_time = time.time()
     print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
     print(f'score map created, time spent: {end_time - start_time}')
@@ -60,6 +108,7 @@ def process_loci_file(filename: str, total_lines: int = 0):
     score_map = read_score_into_ram(_score_file_path, SCORE_LINES)
     counter = 0
     skipped_lines = []
+    print('start processing loci file...')
     with open('./scores_mapping.tsv', 'w') as file_out:
         file_out.write('#chr\tstart\tend\tscore\tfeature')
         with gzip.open(filename, 'rt') as file:
@@ -73,13 +122,15 @@ def process_loci_file(filename: str, total_lines: int = 0):
                 file_out.write(f"{loci}\t{score}\t{feature.get('uniprotFeatureName')}\n")
                 if total_lines:
                     counter += 1
-                    print(f'writing result({100 * counter / total_lines:.2f}%)')
-                if counter >= 30:
-                    break
+                    if counter % (total_lines // 5000) == 0:
+                        ratio = counter / total_lines
+                        time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
+                        print(f'writing result({100 * ratio:.2f}%), estimated time left: {time_left}')
 
     end_time = time.time()
     print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
     print(f'processing complete, time spent: {end_time - start_time}')
 
 
-process_loci_file(_loci_file_path, LOCI_LINES)
+# process_loci_file(_loci_file_path, LOCI_LINES)
+parse_scores(_score_file_path, SCORE_LINES)
