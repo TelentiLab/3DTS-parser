8f2f7eac1576ac6694f7c5985f35692a70ce96bf
Li Yin <liyin@scripps.edu>
2019-04-08 14:12:11 -0700

    Used logger to log info. Execute code in run.py

--- parser.py
+++ parser.py
@@ -2,85 +2,49 @@ import json
 import time
 import gzip
 import datetime
+import logging
 from typing import Dict
 
-_score_file_path = '/Users/yin.li/Downloads/archive/scores.js'
-_loci_file_path = '/Users/yin.li/Downloads/archive/structuralFeatures.loci.js.gz'
-SCORE_LINES = 2273865
-LOCI_LINES = 413021682
 FEATURE_KEYS = ['pdbId', 'pdbChain', 'uniprotFeatureName', 'pdbResidueMin', 'pdbResidueMax']
-"""
-"featureKey": { # this whole object is the key in this file
-    "pdbId": "10GS",
-    "pdbChain": "A",
-    "uniprotFeatureName": "HELIX",
-    "pdbResidueMin": "187",
-    "pdbResidueMax": "194"
-}
-"""
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.INFO)
+_logger = logging.getLogger('3DTS_LOGGER')
 
 
-def parse_scores(filename: str, total_lines: int = 0) -> None:
+def read_scores_into_ram(filename: str, total_lines: int = 0) -> Dict:
+    """
+    Process the scores.gz file and put (feature, score) mapping into RAM. The feature is serialized to a string
+    separated by dot (.) in the format of:
+    pdbId.pdbChain.uniprotFeatureName.pdbResidueMin.pdbResidueMax
+
+    The feature object from the file looks like this:
+    "featureKey": { # this whole object is the key in score file
+        "pdbId": "10GS",
+        "pdbChain": "A",
+        "uniprotFeatureName": "HELIX",
+        "pdbResidueMin": "187",
+        "pdbResidueMax": "194"
+    }
+    :param filename: the full path to the score file
+    :param total_lines: number of lines of the file, optional. If provided, progress and estimated time would be shown.
+    :return: A dictionary containing feature to score mapping.
+    """
     start_time = time.time()
-    counter = 0
-    skipped_lines = []
-    with open('./formatted_scores.tsv', 'w') as file_out:
-        with open(filename, 'r') as file:
-            for line in file:
-                try:
-                    data: Dict = json.loads(line.strip())
-                except:  # error parsing json
-                    skipped_lines.append(line)
-                    continue
-
-                feature = data.get('featureKey')
-                if not feature:
-                    skipped_lines.append(line)
-                    continue
-                key = '.'.join([feature[field] for field in FEATURE_KEYS])
-                try:
-                    post_global_synonymous_rate = data.get(
-                        'nsPostHeptamerIndependentChromosomeSpecificIntergenicRate'
-                    ).get('mean')
-                except AttributeError:  # failed to find score
-                    skipped_lines.append(line)
-                    continue
-                file_out.write(f'{key}\t{post_global_synonymous_rate}\n')
-                if total_lines:
-                    counter += 1
-                    if counter % (total_lines // 1000) == 0:
-                        ratio = counter / total_lines
-                        time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
-                        print(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left}')
-        end_time = time.time()
-        print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
-        print(f'formatted score created, time spent: {end_time - start_time}')
-
-
-def read_score_into_ram(filename: str, total_lines: int = 0) -> Dict:
-    start_time = time.time()
+    _logger.info('starting to read scores into RAM...')
     score_map = {}
     counter = 0
     skipped_lines = []
-    with open(filename, 'r') as file:
+    with gzip.open(filename, 'rt') as file:
         for line in file:
             try:
                 data: Dict = json.loads(line.strip())
             except:  # error parsing json
                 skipped_lines.append(line)
+                _logger.error(f'Failed to parse line into json: {line}')
                 continue
-            """
-            "featureKey": { # this whole object is the key in this file
-                "pdbId": "10GS",
-                "pdbChain": "A",
-                "uniprotFeatureName": "HELIX",
-                "pdbResidueMin": "187",
-                "pdbResidueMax": "194"
-            }
-            """
             feature = data.get('featureKey')
             if not feature:
                 skipped_lines.append(line)
+                _logger.error(f'Failed to get featureKey from line: {line}')
                 continue
             key = '.'.join([feature[field] for field in FEATURE_KEYS])
             try:
@@ -89,6 +53,7 @@ def read_score_into_ram(filename: str, total_lines: int = 0) -> Dict:
                 ).get('mean')
             except AttributeError:  # failed to find score
                 skipped_lines.append(line)
+                _logger.error(f'Failed to get score from line: {line}')
                 continue
             score_map[key] = post_global_synonymous_rate
             if total_lines:
@@ -96,41 +61,48 @@ def read_score_into_ram(filename: str, total_lines: int = 0) -> Dict:
                 if counter % (total_lines // 1000) == 0:
                     ratio = counter / total_lines
                     time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
-                    print(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left}')
+                    _logger.info(f'reading score file({100 * ratio:.2f}%), estimated time left: {time_left}')
     end_time = time.time()
-    print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
-    print(f'score map created, time spent: {end_time - start_time}')
+    _logger.info(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
+    _logger.info(f'score map created, time spent: {end_time - start_time}')
     return score_map
 
 
-def process_loci_file(filename: str, total_lines: int = 0):
+def process_loci_file(loci_filename: str, output_filename: str, score_map: dict, total_lines: int = 0):
     start_time = time.time()
-    score_map = read_score_into_ram(_score_file_path, SCORE_LINES)
+    _logger.info('start processing loci file...')
     counter = 0
     skipped_lines = []
-    print('start processing loci file...')
-    with open('./scores_mapping.tsv', 'w') as file_out:
+    with open(output_filename, 'w') as file_out:
         file_out.write('#chr\tstart\tend\tscore\tfeature')
-        with gzip.open(filename, 'rt') as file:
+        with gzip.open(loci_filename, 'rt') as file:
             for line in file:
-                counter += 1
-                data = json.loads(line.strip())
+                try:
+                    data = json.loads(line.strip())
+                except:  # error parsing json
+                    skipped_lines.append(line)
+                    _logger.error(f'Failed to parse line into json: {line}')
+                    continue
                 feature = data.get('feature')
+                if not feature:
+                    skipped_lines.append(line)
+                    _logger.error(f'Failed to get feature from line: {line}')
+                    continue
                 key = '.'.join([feature.get(k) for k in FEATURE_KEYS])
                 score = score_map.get(key)
-                loci = data.get('locus')
-                file_out.write(f"{loci}\t{score}\t{feature.get('uniprotFeatureName')}\n")
+                if score:
+                    loci = data.get('locus')
+                    file_out.write(f"{loci}\t{score}\t{feature.get('uniprotFeatureName')}\n")
+                else:
+                    # _logger.info(f'current loci does not map to any score: {key}')
+                    skipped_lines.append(line)
                 if total_lines:
                     counter += 1
                     if counter % (total_lines // 5000) == 0:
                         ratio = counter / total_lines
                         time_left = datetime.timedelta(seconds=(time.time() - start_time) * (1 - ratio) / ratio)
-                        print(f'writing result({100 * ratio:.2f}%), estimated time left: {time_left}')
+                        _logger.info(f'writing result({100 * ratio:.2f}%), estimated time left: {time_left}')
 
     end_time = time.time()
-    print(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
-    print(f'processing complete, time spent: {end_time - start_time}')
-
-
-# process_loci_file(_loci_file_path, LOCI_LINES)
-parse_scores(_score_file_path, SCORE_LINES)
+    _logger.info(f'# of lines skipped due to parsing error: {len(skipped_lines)}')
+    _logger.info(f'processing complete, time spent: {end_time - start_time}')
---
+++ run.py
@@ -0,0 +1,10 @@
+from parser import read_scores_into_ram, process_loci_file
+
+_score_file_path = '/Users/yin.li/Downloads/archive/scores.js.gz'
+_loci_file_path = '/Users/yin.li/Downloads/archive/structuralFeatures.loci.js.gz'
+_result_file_path = '/Users/yin.li/Downloads/archive/3dts_scores.tsv'
+SCORE_LINES = 2273865
+LOCI_LINES = 413021682
+
+score_map = read_scores_into_ram(_score_file_path, SCORE_LINES)
+process_loci_file(_loci_file_path, _result_file_path, score_map, LOCI_LINES)
